f9384965e9066646f300c6481f014155098cc1a3
maksimkla19 <68090252+maksimkla19@users.noreply.github.com>
2023-11-05 20:01:03 +0600

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 require_relative 'train'
 require_relative 'train_cargo'
-require_relative 'train_passenger'
+require_relative 'train_passanger'
 require_relative 'station'
 require_relative 'route'
 require_relative 'wagon'
